f432d35eeb1da86512d9f771859ecd3c29aa03da
남태희 <68994412+NamTaeHee1@users.noreply.github.com>
2021-07-29 19:55:51 +0900

    [ADD] PlayerMovement
    [ADD] PlayerMovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float h = 0;
+    private float v = 0;
+
+    private Transform PlayerTransform;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
