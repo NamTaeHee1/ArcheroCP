01a6461194c8706d1880c131b3dc4ea31308c39c
남태희 <68994412+NamTaeHee1@users.noreply.github.com>
2021-07-31 02:32:12 +0900

    [UPDATE] CameraControl
    [UPDATE] CameraControl

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -5,14 +5,8 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     [SerializeField]    private GameObject Target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
