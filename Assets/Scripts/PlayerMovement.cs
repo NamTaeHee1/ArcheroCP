45186deebb783ecfb3fc1cb9864d0ba252714426
남태희 <68994412+NamTaeHee1@users.noreply.github.com>
2021-07-30 19:35:19 +0900

    [UPDATE] PlayerMovement
    [UPDATE] PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,17 +6,31 @@ public class PlayerMovement : MonoBehaviour
 {
     private float h = 0;
     private float v = 0;
+    private float MoveSpeed = 3.0f;
 
     private Transform PlayerTransform;
 
-    void Start()
+    private void Awake() 
     {
-        
+            PlayerTransform = GetComponent<Transform>();
     }
 
 
     void Update()
     {
+        PlayerMove();
+    }
+
+    private void GetAxis()
+    {
         
+    }
+
+    private void PlayerMove()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        PlayerTransform.Translate(new Vector3(h, 0, v) * MoveSpeed * Time.del);
     }
 }
