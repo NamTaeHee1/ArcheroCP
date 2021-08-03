217543a078d1bb1e2879ed7c44452acc2346998e
남태희 <68994412+NamTaeHee1@users.noreply.github.com>
2021-08-04 01:22:58 +0900

    [UPDATE] JoyStickControl
    [UPDATE] JoyStickControl

--- Assets/Scripts/JoyStickControl.cs
+++ Assets/Scripts/JoyStickControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class JoyStickControl : MonoBehaviour
 {
@@ -11,13 +12,27 @@ public class JoyStickControl : MonoBehaviour
     private Vector3 JoyStickVec;
     private Vector3 JoyStickFirstPosition;
 
-    private void Awake() 
+    private void Start() 
     {
+        Radius = GetComponent<RectTransform>().sizeDelta.x * 0.5f;
         JoyStickFirstPosition = Stick.transform.position;
     }
 
-    public void Drag()
+    public void Drag(BaseEventData _Data)
     {
+        PointerEventData Data = _Data as PointerEventData;
+        Vector3 Position = Data.position;
 
+        JoyStickVec = (Position - JoyStickFirstPosition).normalized;
+
+        float Distance = Vector3.Distance(Position, JoyStickFirstPosition);
+
+        Stick.position = JoyStickFirstPosition + JoyStickVec * (Distance < Radius ? Distance : Radius);
+    }
+
+    public void DragEnd()
+    {
+        Stick.position = JoyStickFirstPosition;
+        JoyStickVec = Vector3.zero;
     }
 }
