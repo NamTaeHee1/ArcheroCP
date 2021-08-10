18476d27a783a3db2e571248a9bd489f8732ef1b
남태희 <68994412+NamTaeHee1@users.noreply.github.com>
2021-08-11 02:36:12 +0900

    [UPDATE] PlayerTargeting
    [UPDATE] PlayerTargeting

--- Assets/Scripts/PlayerTargeting.cs
+++ Assets/Scripts/PlayerTargeting.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class PlayerTargeting : MonoBehaviour
 {
     private PlayerTargeting instance;
-    public List<GameObject> EnemyList = new List<GameObject>();
+    public Collider[] HitEnemyColliders;
 
-        private void Update() {
-            for(int i = 0 ; i < EnemyList.Count; i++)
-                print(EnemyList[i].name);
-        }
+    private LayerMask EnemyLayerMask;
+
+    private void Awake() 
+    {
+        EnemyLayerMask = LayerMask.GetMask("Enemy");
+    }
+
+    private void FixedUpdate() 
+    {
+        CheckEnemyCollider();
+        for(int i = 0; i < HitEnemyColliders.Length; i++)
+            print(HitEnemyColliders[i].transform.name);
+    }
+
+    private void CheckEnemyCollider()
+    {
+        HitEnemyColliders = Physics.OverlapSphere(transform.position, 6.0f, EnemyLayerMask);
+    }
+
 }
